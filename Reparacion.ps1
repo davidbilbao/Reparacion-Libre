9ef5994e44a38fc19ec861778d7326664d7b9a5e
davidbilbao <50782623+davidbilbao@users.noreply.github.com>
2023-08-31 23:23:53 -0500

    Update Reparacion.ps1

--- Reparacion.ps1
+++ Reparacion.ps1
@@ -1,5 +1,199 @@
- function Borrar-Particion {
-    $disco = Read-Host "Ingrese el numero del disco"
+Add-Type -AssemblyName PresentationFramework
+
+function Show-Menu {
+	do{
+    $options = @(
+        "Diagnóstico y reparación del sistema",
+        "Escaneo y reparación de disco",
+        "Opciones de inicio",
+        "Hacer un respaldo",
+        "Actualizar programas",
+        "Todas las características del sistema",
+        "Crear particiones",
+        "Reparar Windows Update",
+        "Restaurar el sistema",
+        "Borrar archivos basura",
+        "Instalar programa",
+        "Borrar particiones",
+        "Formatear particiones",
+        "Información del hardware de la computadora",
+        "Información de los drivers instalados",
+        "Desfragmentación del disco",
+        "Optimizar sistema",
+        "Salir"
+    )
+
+    $choice = $options | Out-GridView -Title "Menú" -PassThru
+
+    switch ($choice) {
+        "Diagnóstico y reparación del sistema" { 
+		Clear-Host
+		Realizar-Diagnostico 
+		}
+        "Escaneo y reparación de disco" { 
+		Clear-Host
+		Escaneo-Reparacion-Disco 
+		}
+        "Opciones de inicio"{
+			Clear-Host
+			msconfig
+			}
+        "Hacer un respaldo"{
+			Clear-Host
+			respaldo
+			}
+         "Actualizar programas"{
+			 Clear-Host
+            winget upgrade
+			winget upgrade --all
+          }
+          "Todas las características del sistema"{
+			  Clear-Host
+			  infor
+		  }
+			"Crear particiones"{
+				Clear-Host
+				crear-part
+			}
+			"Reparar Windows Update"{
+				Clear-Host
+				# Detener los servicios wuauserv y bits
+				Stop-Service -Name wuauserv, bits -Force
+
+				# Iniciar los servicios wuauserv y bits	
+				Start-Service -Name wuauserv, bits
+			}
+			"Restaurar el sistema"{
+				Clear-Host
+				rstrui
+			}
+			"Borrar archivos basura"{
+				Clear-Host
+				Start-Process "cleanmgr"
+				Remove-Item -Path $env:TEMP\* -Force -Recurse
+			}
+			 "Instalar programa"{
+				Clear-Host
+				$programa = Read-Host "Ingrese el nombre del programa que desea instalar"
+				Start-Process "winget" -ArgumentList "install $programa" -Wait
+			}
+			"Borrar particiones"{
+				Clear-Host
+				Borrar-Particion
+			}
+			"Formatear particiones"{ 
+		
+            Clear-Host
+			Formatear-Particion
+			}
+			"Información del hardware de la computadora"{
+				Clear-Host
+				infor
+			}
+			"Información de los drivers instalados"{
+				Clear-Host
+				dri
+			}
+			 "Desfragmentación del disco"{
+				Clear-Host
+				des
+			}
+        "Salir" {
+				Clear-Host	
+				Write-Host "Saliendo..." 
+				}
+		"Optimizar sistema"{
+				Clear-Host	
+				# Abrir Propiedades del Sistema
+				Start-Process "control" -ArgumentList "sysdm.cpl"
+
+				# Abrir Editor del Registro
+				Start-Process "regedit"
+
+				# Abrir Liberador de Espacio en Disco
+				Start-Process "cleanmgr"
+			}
+    }
+	}while($choice -ne "Salir")
+}
+function des{
+	
+	$options = @(
+        "Si"
+		"No"
+    )
+	
+
+    "Si"{
+        $dis = Read-Host "Ingrese la letra del disco que desea desfragmentar"
+        defrag $dis /O
+	}
+	"No"{
+		Clear-Host
+	}
+    
+}
+function dri{
+		# Obtener información de los drivers
+		Get-WmiObject Win32_PnPSignedDriver | Select-Object Manufacturer, DeviceName, DriverVersion
+
+		# Ejecutar DirectX Diagnostic Tool (dxdiag)
+		Start-Process "dxdiag"
+}
+function infor{
+	
+	# Obtener información del sistema
+	systeminfo
+
+	# Obtener información de la CPU
+	Get-WmiObject Win32_Processor | Select-Object Caption, DeviceID
+
+	# Obtener información de las memorias
+	Get-WmiObject Win32_PhysicalMemory
+
+	# Obtener información de las unidades de disco
+	Get-WmiObject Win32_DiskDrive
+
+	# Obtener información de la placa base
+	Get-WmiObject Win32_BaseBoard
+
+	# Obtener información del sistema operativo
+	(Get-WmiObject Win32_OperatingSystem).Caption, (Get-WmiObject Win32_OperatingSystem).Version
+	
+}
+function Formatear-Particion {
+    $disco = Read-Host "Ingrese el número del disco"
+    
+    $diskpartScript = @"
+    select disk $disco
+    clean
+    create partition primary
+    select partition 1
+    active
+    format fs=NTFS
+    assign
+"@
+
+    $diskpartScript | diskpart
+
+    Write-Host "Partición formateada exitosamente."
+}
+function Realizar-Diagnostico {
+   
+    Write-Host "Realizando diagnóstico y reparación del sistema..."
+
+
+    # Ejecutar comandos de diagnostico y reparacion
+    sfc /scannow
+    DISM /Online /Cleanup-Image /CheckHealth
+    DISM /Online /Cleanup-Image /ScanHealth
+    DISM /Online /Cleanup-Image /RestoreHealth
+
+
+}
+function Borrar-Particion {
+    $disco = Read-Host "Ingrese el nombre del disco"
+	Invoke-Expression "list partition"
     $particion = Read-Host "Ingrese el número de la partición a borrar"
 
     $diskpartScript = @"
@@ -11,6 +205,18 @@
     $diskpartScript | diskpart
 
     Write-Host "Partición borrada exitosamente."
+}
+function Escaneo-Reparacion-Disco {
+    $disco = Read-Host "Ingrese el nombre del disco"
+	Invoke-Expression "chkdsk $disco /f /r"
+    Write-Host "Realizando escaneo y reparación de disco..."
+}
+function respaldo{
+            diskpart
+			Get-Volume
+
+			$respaldo = Read-Host "Ingrese la letra del respaldo"
+			wbadmin start backup -backuptarget:$respaldo -include:C: -allCritical
 }
 function infor{
 	# Obtener información del sistema
@@ -52,172 +258,5 @@ function crear-part{
 	$let = Read-Host "Ingrese la letra de unidad que desea asignar a la partición"
 	Invoke-Expression "assign letter=$let"
 }
-function Formatear-Particion {
-    $disco = Read-Host "Ingrese el número del disco"
-    
-    $diskpartScript = @"
-    select disk $disco
-    clean
-    create partition primary
-    select partition 1
-    active
-    format fs=NTFS
-    assign
-"@
-
-    $diskpartScript | diskpart
-
-    Write-Host "Partición formateada exitosamente."
-}
-
-function Realizar-Diagnostico {
-    Clear-Host
-    Write-Host "Realizando diagnóstico y reparación del sistema..."
-
-    # Ejecutar comandos de diagnostico y reparacion
-    sfc /scannow
-    DISM /Online /Cleanup-Image /CheckHealth
-    DISM /Online /Cleanup-Image /ScanHealth
-    DISM /Online /Cleanup-Image /RestoreHealth
-
-    Write-Host "diagnostico completado"
-}
-function Desfragmentar-Disco {
-    Write-Host "Advertencia: Esto es solo para discos duros mecanicos"
-    $choice = Read-Host "¿Desea continuar? Ingrese '1' para si, '2' para no"
-
-    if ($choice -eq "1") {
-        $dis = Read-Host "Ingrese la letra del disco que desea desfragmentar"
-        defrag $dis /O
-    }
-}
-function Show-Menu {
-    Clear-Host
-    Write-Host "------------- Menú -------------"
-    Write-Host "1. Diagnóstico y reparación del sistema"
-    Write-Host "2. Escaneo y reparación de disco"
-    Write-Host "3. Opciones de inicio"
-    Write-Host "4. Hacer un respaldo"
-    Write-Host "5. Actualizar programas"
-    Write-Host "6. Todas las características del sistema"
-    Write-Host "7. Crear particiones"
-    Write-Host "8. Reparar Windows Update"
-    Write-Host "9. Restaurar el sistema"
-    Write-Host "10. Borrar archivos basura"
-    Write-Host "11. Instalar programa"
-    Write-Host "12. Borrar particiones"
-    Write-Host "13. Formatear particiones"
-    Write-Host "14. Información del hardware de la computadora"
-    Write-Host "15. Información de los drivers instalados en la computadora"
-    Write-Host "16. Desfragmentación del disco"
-    Write-Host "17. Optimizar sistema"
-    Write-Host "18. Salir"
-}
-
-do {
-    Show-Menu
-    $choice = Read-Host "Elige una opción"
-    switch ($choice) {
-        "1" { # Diagnosticco y reparacion del sistema
-			Clear-Host
-            Realizar-Diagnostico
-			
-        }
-        "2" { # Escaneo y reparación de disco
-            Clear-Host
-			
-			$disco = Read-Host "Ingrese el nombre del disco"
-			Invoke-Expression "chkdsk $disco /f /r"
-
-        }
-        "3" { # Opciones de inicio
-            Clear-Host
-            msconfig
-        }
-        "4" { # Hacer un respaldo
-            Clear-Host
-			diskpart
-			Get-Volume
-
-			$respaldo = Read-Host "Ingrese la letra del respaldo"
-			wbadmin start backup -backuptarget:$respaldo -include:C: -allCritical
-        }
-        "5" { # Actualizar programas
-            Clear-Host
-			winget upgrade
-			winget upgrade --all
-        }
-        "6" { # Todas las características del sistema
-            Clear-Host
-			Get-WmiObject Win32_PnPSignedDriver
-			systeminfo
-        }
-        "7" { # Crear particiones
-            Clear-Host
-			crear-part
-        }
-        "8" { # Reparar Windows Update
-            Clear-Host
-
-        }
-        "9" { # Restaurar el sistema
-		    Clear-Host
-            rstrui
-        }
-        "10" { # Borrar archivos basura
-			Clear-Host
-			Start-Process "cleanmgr"
-			Remove-Item -Path $env:TEMP\* -Force -Recurse
-        }
-        "11" { # Instalar programa
-            Clear-Host
-			$programa = Read-Host "Ingrese el nombre del programa que desea instalar"
-			Start-Process "winget" -ArgumentList "install $programa" -Wait
-        }
-        "12" { # Borrar particiones
-			Clear-Host
-            Borrar-Particion
-        }
-        "13" { # Formatear particiones
-		
-            Clear-Host
-			Formatear-Particion
-        }
-        "14" { # Información del hardware de la computadora
-            Clear-Host
-			infor
-
-        }
-        "15" { # Información de los drivers instalados en la computadora
-            Clear-Host
-			# Obtener información de los drivers
-			Get-WmiObject Win32_PnPSignedDriver | Select-Object Manufacturer, DeviceName, DriverVersion
-
-			# Ejecutar DirectX Diagnostic Tool (dxdiag)
-			Start-Process "dxdiag"
-        }
-        "16" { # Desfragmentación del disco
-            Clear-Host
-            Desfragmentar-Disco
-        }
-        "17" { # Optimizar sistema
-            Clear-Host
-			# Abrir Propiedades del Sistema
-			Start-Process "control" -ArgumentList "sysdm.cpl"
-
-			# Abrir Editor del Registro
-			Start-Process "regedit"
-
-			# Abrir Liberador de Espacio en Disco
-			Start-Process "cleanmgr"
-        }
-        "18" { # Salir
-            break
-        }
-        default {
-            Write-Host "Opción inválida. Por favor, elige una opción válida."
-        }
-    }
-    Pause
-} while ($choice -ne "18")
-
+# Ejecutar el menú
+Show-Menu
